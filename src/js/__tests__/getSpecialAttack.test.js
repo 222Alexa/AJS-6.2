6b82306d1939de43cc795b524b88a03783ec17cc
Alexandra Titova <auto-steklo@mail.ru>
2021-02-24 19:00:23 +0300

    destructuring

---
+++ src/js/__tests__/getSpecialAttack.test.js
@@ -0,0 +1,41 @@
+import getSpecialAttack from '../getSpecialAttack';
+
+test('Добавление дефолтного значения в описание, если оно недоступно', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    deffence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+  const expected = getSpecialAttack(character);
+  const result = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  expect(expected).toEqual(result);
+});
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import getSpecialAttack from './getSpecialAttack';
+
+console.log(getSpecialAttack);
